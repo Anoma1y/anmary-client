6b193a95237861663ba3c5ea506528a2c191555f
German Voytekhovich <ueuknjg@gmail.com>
2018-09-06 21:26:33 +0300

    fix

--- app/containers/Admin/containers/Sidebar/components/Navigation/index.js
+++ app/containers/Admin/containers/Sidebar/components/Navigation/index.js
@@ -11,16 +11,15 @@ import {
 import Storage from 'lib/storage';
 
 const NAVIGATION_MENU = [
-  { id: 1, role_name: 'operations-list', name: 'Товары', icon: <AccountBalanceIcon />, link: '/admin/products' },
-  { id: 2, role_name: 'shift-list', name: 'Пользователи', icon: <CardMembershipIcon />, link: '/admin/users' },
+  { id: 1, role_name: 'products-list', name: 'Товары', icon: <AccountBalanceIcon />, link: '/admin/products' },
+  { id: 2, role_name: 'users-list', name: 'Пользователи', icon: <CardMembershipIcon />, link: '/admin/users' },
   { id: 3, role_name: 'roles-list', name: 'Роли', icon: <AssessmentIcon />, link: '/admin/roles' },
-  { id: 4, role_name: 'users-list', name: 'Категории', icon: <SupervisorAccountIcon />, link: '/admin/categories' },
-  { id: 5, role_name: 'categories-list', name: 'Бренды', icon: <InboxIcon />, link: '/admin/brands' },
-  { id: 6, role_name: 'export-list', name: 'Сезоны', icon: <ImportExportIcon />, link: '/admin/seasons' }
+  { id: 4, role_name: 'categories-list', name: 'Категории', icon: <SupervisorAccountIcon />, link: '/admin/categories' },
+  { id: 5, role_name: 'brands-list', name: 'Бренды', icon: <InboxIcon />, link: '/admin/brands' },
+  { id: 6, role_name: 'seasons-list', name: 'Сезоны', icon: <ImportExportIcon />, link: '/admin/seasons' }
 ];
 
 export default class Navigation extends Component {
-
   renderItem = (item) => (
     <div className={'sidebar-item sidebar-navigation_item'} key={item.id}>
       <div className={'sidebar-item_icon'}>
