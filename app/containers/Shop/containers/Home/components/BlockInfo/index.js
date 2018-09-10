91ee6c89a02ca8d79df271022cc5977946e92de6
German Voytekhovich <ueuknjg@gmail.com>
2018-09-10 22:20:42 +0300

    fixed style for blocks

--- app/containers/Shop/containers/Home/components/BlockInfo/index.js
+++ app/containers/Shop/containers/Home/components/BlockInfo/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {
-  Add as AddIcon
+  Add as AddIcon,
+  Notifications as NotificationsIcon
 } from '@material-ui/icons';
 
 export default class BlockInfo extends Component {
@@ -8,51 +9,62 @@ export default class BlockInfo extends Component {
     return (
       <section>
         <div className={'information-blocks'}>
-          <div className="information-block new_collections">
-            <div className="collection_info">
-              <div className="collection_name">
-                <h2>Осень/Зима</h2>
-                <h2>2017/2018</h2>
-                <p>Новое поступление</p>
+          <div className={'information-block new_collections'}>
+            <a href={'#'} className={'new_collections_image'}>
+              <div className={'collection_info'}>
+                <div className={'collection_name'}>
+                  <h2>Осень/Зима</h2>
+                  <h2>2017/2018</h2>
+                  <p>Новое поступление</p>
+                </div>
               </div>
-              <a href={''} className={'information-block_link information-block_link__black'}>Смотреть коллекцию</a>
-            </div>
-            <div className={'information-block_overlay'} />
+              <div className={'information-block_overlay'} />
+            </a>
           </div>
-          <div className="information-block season_sale">
-            <a href={'#'} className="season_sale_info">
+          <div className={'information-block season_sale'}>
+            <a href={'#'} className={'season_sale_info'}>
               <p>Сезонная распродажа</p>
               <h1>Sale</h1>
             </a>
           </div>
           <div className={'information-block new_arrivals'}>
-            <div className={'information-block_overlay'} />
-            <div className={'arrivals_info'}>
-              <h1>Последнее поступление</h1>
-              <a href={'#'} className={'information-block_link information-block_link__black'}>Смотреть</a>
-            </div>
+
+            <a href={'#'} className={'new_arrivals_image'}>
+              <div className={'arrivals_info'}>
+                <div className={'collection_name'}>
+                  <h1>Последнее поступление</h1>
+                </div>
+              </div>
+              <div className={'information-block_overlay'} />
+            </a>
           </div>
           <div className={'information-block subscribe'}>
             <div className={'information-block_overlay'} />
-            <div className={'get_subscribe'}>
-              <h2>Подписаться на рассылку</h2>
-            </div>
-            <div className={'formSubscribe'}>
-              <input
-                type="text" placeholder="Введите E-Mail" id="get_subscribe_email"
-                name="get_subscribe_email" />
-              <button id="get_subscribe_btn" name="get_subscribe_btn">
-                <AddIcon />
-              </button>
-              <span id="error-subscribe" />
-            </div>
+
+            <form action={'#'} className={'subscribe-form'}>
+              <div className={'subscribe-form_input'}>
+                <input
+                  type={'email'}
+                  placeholder="email@example.com"
+                />
+                <span className={'subscribe-form_error'}> </span>
+              </div>
+              <div className={'subscribe-form_btn'}>
+                <button>
+                  Подписаться
+                  <NotificationsIcon />
+                </button>
+              </div>
+            </form>
+
           </div>
-          <div className="information-block all_catalog">
-            <div className={'information-block_overlay'} />
-            <div className="catalog_info">
-              <h2>Большой выбор женской одежды</h2>
-            </div>
-            <a href={'#'} className={'information-block_link information-block_link__black'}>Перейти в каталог</a>
+          <div className={'information-block all_catalog'}>
+            <a href={'#'} className={'all_catalog_image'}>
+              <div className={'catalog_info'}>
+                <h2>Большой выбор женской одежды</h2>
+              </div>
+              <div className={'information-block_overlay'} />
+            </a>
           </div>
         </div>
       </section>
