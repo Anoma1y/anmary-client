5433927ea6cd71242ecb8173687a0212266d627e
German Voytekhovich <ueuknjg@gmail.com>
2018-09-13 23:30:11 +0300

    fix

--- app/containers/Shop/containers/Products/containers/List/index.js
+++ app/containers/Shop/containers/Products/containers/List/index.js
@@ -1,17 +1,59 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { 
-  Grid
+import {
+  Grid,
+  ExpansionPanel,
+  ExpansionPanelDetails,
+  ExpansionPanelSummary,
+  ExpansionPanelActions,
 } from '@material-ui/core';
-
+import {
+  ExpandMore as ExpandMoreIcon
+} from '@material-ui/icons';
 
 @connect(({ Shop_Products, Shop_Products_List }) => ({ Shop_Products, Shop_Products_List }))
 export default class List extends Component {
   render() {
     return (
-      <div>
+      <Grid container className={'shop products'}>
 
-      </div>
+        <Grid item xs={12}>
+
+          <Grid container>
+
+            <Grid item sm={12} xs={3}>
+
+              <div className={'product-filter'}>
+
+                <div className={'product-filter_item'}>
+
+                  <ExpansionPanel>
+                    <ExpansionPanelSummary
+                      expanded
+                      expandIcon={<ExpandMoreIcon />}
+                    >
+                      Категории
+                    </ExpansionPanelSummary>
+                    <ExpansionPanelDetails>
+                      Трусики
+                    </ExpansionPanelDetails>
+                  </ExpansionPanel>
+
+                </div>
+
+              </div>
+
+            </Grid>
+
+            <Grid item sm={12} xs={9}>
+
+            </Grid>
+
+          </Grid>
+
+        </Grid>
+
+      </Grid>
     );
   }
 }
