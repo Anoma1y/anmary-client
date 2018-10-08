706def5edeae32700f3a502e47e727747b18775b
German <ueuknjg@gmail.com>
2018-10-08 17:38:53 +0300

    uodate footer

--- app/containers/Shop/containers/Footer/index.js
+++ app/containers/Shop/containers/Footer/index.js
@@ -1,39 +1,96 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
 import './style.scss';
 
+@connect(({ routing }) => ({ routing }))
 export default class Footer extends Component {
   render() {
+    const { pathname } = this.props.routing.location;
+
     return (
       <footer className={'footer'}>
-        <div className={'footer_logo'}>
-          <a href={'#'}>Anmary</a>
+        <div className={'footer-logo'}>
+          <a
+            href={'#'}
+            className={'footer-logo_link'}
+          >
+            Anmary
+          </a>
         </div>
-        <nav className={'footer_nav'}>
-          <ul>
-            <li><Link to={'/'}>Главная</Link></li>
-            <li><Link to={'/product'}>Каталог</Link></li>
-            <li><Link to={'/news'}>Новости</Link></li>
-            <li><Link to={'/contact'}>Контакты</Link></li>
+        <nav className={'footer-nav'}>
+          <ul className={'footer-nav-list'}>
+            <li className={'footer-nav-list_item'}>
+              <Link
+                to={'/'}
+                className={`footer-nav_link${pathname === '/' ? ' footer-nav_link__active' : ''}`}
+              >
+                Главная
+              </Link>
+            </li>
+            <li className={'footer-nav-list_item'}>
+              <Link
+                to={'/product'}
+                className={`footer-nav_link${pathname === '/product' ? ' footer-nav_link__active' : ''}`}
+              >
+                Каталог
+              </Link>
+            </li>
+            <li className={'footer-nav-list_item'}>
+              <Link
+                to={'/news'}
+                className={`footer-nav_link${pathname === '/news' ? ' footer-nav_link__active' : ''}`}
+              >
+                Новости
+              </Link>
+            </li>
+            <li className={'footer-nav-list_item'}>
+              <Link
+                to={'/contact'}
+                className={`footer-nav_link${pathname === '/contact' ? ' footer-nav_link__active' : ''}`}
+              >
+                Контакты
+              </Link>
+            </li>
           </ul>
         </nav>
-        <div className={'footer_pay'}>
-          <ul>
-            <li>
-              <img src={'/static/icons/mastercard.svg'} alt={'Master Card'} />
+        <div className={'footer-pay'}>
+          <ul className={'footer-pay-list'}>
+            <li className={'footer-pay-list_item'}>
+              <img
+                src={'/static/icons/mastercard.svg'}
+                alt={'Master Card'}
+                className={'footer-pay_img'}
+              />
             </li>
-            <li>
-              <img src={'/static/icons/visa.svg'} alt={'Visa'} />
+            <li className={'footer-pay-list_item'}>
+              <img
+                src={'/static/icons/visa.svg'}
+                alt={'Visa'}
+                className={'footer-pay_img'}
+              />
             </li>
-            <li>
-              <img src={'/static/icons/mir.svg'} alt={'MIR'} />
+            <li className={'footer-pay-list_item'}>
+              <img
+                src={'/static/icons/mir.svg'}
+                alt={'MIR'}
+                className={'footer-pay_img'}
+              />
             </li>
-            <li>
-              <img src={'/static/icons/apple-pay.svg'} alt={'Apple Pay'} />
+            <li className={'footer-pay-list_item'}>
+              <img
+                src={'/static/icons/apple-pay.svg'}
+                alt={'Apple Pay'}
+                className={'footer-pay_img'}
+              />
             </li>
-            <li>
-              <img src={'/static/icons/wifi.png'} alt={'PayPass'} />
+            <li className={'footer-pay-list_item'}>
+              <img
+                src={'/static/icons/wifi.png'}
+                alt={'PayPass'}
+                className={'footer-pay_img'}
+              />
             </li>
           </ul>
         </div>
